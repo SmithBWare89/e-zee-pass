36d2f2c40eab719621493a2826e84321a13932c6
Stephon Smith <smithwrestling89@gmail.com>
2020-06-20 21:39:01 -0400

    established initial game functionality and pseudocode

---
+++ readme.md
@@ -0,0 +1,4 @@
+### Established basic functionality
+- Wrote pseudocode to establish initial generator functionality
+- Established basic functions for each random character to be generated
+- Declared variables to store prompt answers
--- script.js
+++ script.js
@@ -12,3 +12,44 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Click button to trigger to accept inputs
+
+  // Accept user input for length (8 - 128)
+  var lengthConfirm = prompt("How long would you like your password to be?");
+  lengthConfirm = parseInt(lengthConfirm);
+  
+  // Confirm is user wants lowercase letters
+  var lowerConfirm = confirm("Would you like lowercase letters in your password?");
+    // If yes
+      // Generate randomLowercase
+      function randomLower(){}  
+    // If no
+      // Move to next prompt
+    
+  // Confirm if user wants uppercase letters
+  var upperConfirm = confirm("Would you like uppercase letters in your password??");
+    // If yes
+      // Generate randomUppercase
+      function randomUpper(){}
+    // If no
+      // Move to next prompt
+
+  // Confirm if user wants numbers
+  var numConfirm = confirm("Would you like numbers in your password?");
+    // If yes
+      // Generate randomNumber
+      function randomNumber(){}
+    
+
+  // Confirm if user wants special characters
+  var specialConfirm = confirm("Would you like special characters in your password?");
+    // If yes
+      // Generate randomSpecialChar
+      function randomSpecChar(){}
+    // If no
+      // Finish prompts
+  
+  // Generate password
+    // Print password in text area
+
