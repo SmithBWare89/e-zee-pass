66d00eae77debb4d2c86c6f37e31b5471298dc44
Stephon Smith <smithwrestling89@gmail.com>
2020-06-21 22:01:59 -0400

    initial game functionality

--- script.js
+++ script.js
@@ -1,53 +1,12 @@
-debugger;
-// Assignment Code
+  
+// Assignment code here
+
+
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  // Accept user input for length (8 - 128)
-  var lengthConfirm = prompt("How long would you like your password to be? (Between 8 and 128 characters)");
-  lengthConfirm = parseInt(lengthConfirm);
-    if (isNaN(lengthConfirm)) {
-      alert("Please enter a valid number!");
-      writePassword();
-    } else if (lengthConfirm < 8 || lengthConfirm > 128) {
-      alert("Your password length must be between 8 and 128 characters!");
-      writePassword();
-    } else {
-      answer.passLength = lengthConfirm;
-    }
-  // Confirm is user wants lowercase letters
-  var lowerConfirm = confirm("Would you like lowercase letters in your password?");
-      if (lowerConfirm) {
-        answer.lower = true;
-      } else {
-        answer.lower = false;
-        return;
-      }
-  // Confirm if user wants uppercase letters
-  var upperConfirm = confirm("Would you like uppercase letters in your password??");
-      if (upperConfirm) {
-        answer.upper = true;
-      } else {
-        answer.upper = true;
-      }
-  // Confirm if user wants numbers
-  var numConfirm = confirm("Would you like numbers in your password?");
-      if (numConfirm) {
-        answer.number = true;
-      } else {
-        answer.number = false;
-      }
-  // Confirm if user wants special characters
-  var specialConfirm = confirm("Would you like special characters in your password?");
-      if (specialConfirm) {
-        answer.special = true;
-      } else {
-        answer.special = false;
-      }
-  
-  generatePassword(answer.lower, answer.upper, answer.number, answer.special);
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -58,50 +17,61 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var answer = {
-  password:[],
-};
 
-function generatePassword(lower, upper, number, special) {
-  while (answer.passLength > 0) {
-    if (lower && answer.passLength > 0) {
-      randomLower();
-    } if (upper && answer.passLength > 0) {
-      randomUpper();
-    } if (number && answer.passLength > 0) {
-      randomNumber();
-    } if (special && answer.passLength > 0) {
-      randomSpecChar();
-    } else {
-      alert("Cannot generate a password without parameters. Please try again.")
-      break;
-    }
-  }
-  return answer.password.join(''); 
-}
+  // Accept user input for length (8 - 128)
+  var lengthConfirm = prompt("How long would you like your password to be?");
+  lengthConfirm = parseInt(lengthConfirm);
 
-// Generate randomLowercase
-function randomLower(){
-  var lowerLetter = String.fromCharCode(Math.floor(Math.random() * 26 + 97));
-  answer.passLength = answer.passLength - 1;
-  answer.password.push(lowerLetter);
-}  
+  // Confirm is user wants lowercase letters
+  var lowerConfirm = confirm("Would you like lowercase letters in your password?");
+    // If yes
+      // Generate randomLowercase
+      function randomLower(){}  
+    // If no
+      function randomLower(){
+        var lowerLetter = String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+        return lowerLetter;
+      }  
+    // else
+      // Move to next prompt
 
-function randomUpper(){
-  var upperLetter = String.fromCharCode(Math.floor(Math.random() * 26 + 65));
-  answer.passLength = answer.passLength - 1;
-  answer.password.push(upperLetter);
-}
+  // Confirm if user wants uppercase letters
+  var upperConfirm = confirm("Would you like uppercase letters in your password??");
+    // If yes
+      // Generate randomUppercase
+      function randomUpper(){}
+    // If no
+      function randomUpper(){
+        var upperLetter = String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+        return upperLetter;
+      }
+    // else
+      // Move to next prompt
 
-function randomNumber(){
-  var number = String.fromCharCode(Math.floor(Math.random() * 10 + 48));
-  answer.passLength = answer.passLength - 1;
-  answer.password.push(number);
-}
+  // Confirm if user wants numbers
+  var numConfirm = confirm("Would you like numbers in your password?");
+    // If yes
+      // Generate randomNumber
+      function randomNumber(){}
+      function randomNumber(){
+        var number = String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+        return number;
+      }
+    // else
+      // Move to the next prompt
 
-function randomSpecChar(){
-  var specialCharList = ["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","'\'","]","^","_","`","{","|","}","~"];
-  var specialChar = Math.floor(Math.random() * specialCharList.length);
-  answer.passLength = answer.passLength - 1;
-  answer.password.push(specialChar);
-}
+
+  // Confirm if user wants special characters
+  var specialConfirm = confirm("Would you like special characters in your password?");
+    // If yes
+      // Generate randomSpecialChar
+      function randomSpecChar(){}
+    // If no
+      function randomSpecChar(){
+        var specialChar = ["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","'\'","]","^","_","`","{","|","}","~"];
+        return specialChar[Math.floor(Math.random() * specialChar.length)];
+      }
+    // else
+      // Finish prompts
+
+  // Generate password
